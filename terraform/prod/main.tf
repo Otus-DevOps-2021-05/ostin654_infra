e9facf4717c7fcec0d843fe34c9caa31c0035695
ostin654 <ostin654@gmail.com>
2021-07-04 23:51:08 +0300

    Terraform modules, backend s3, provisioners (try to fix tests)

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -6,23 +6,23 @@ provider "yandex" {
 }
 
 module "app" {
-  name = "reddit-app"
-  source          = "../modules/app"
+  name           = "reddit-app"
+  source         = "../modules/app"
   ssh_public_key = var.ssh_public_key
-  disk_image  = var.app_disk_image
-  subnet_id       = var.yc_subnet_id
-  cores = 4
-  memory = 4
-  core_fraction = 5
+  disk_image     = var.app_disk_image
+  subnet_id      = var.yc_subnet_id
+  cores          = 4
+  memory         = 4
+  core_fraction  = 5
 }
 
 module "db" {
-  name = "reddit-db"
-  source          = "../modules/db"
+  name           = "reddit-db"
+  source         = "../modules/db"
   ssh_public_key = var.ssh_public_key
-  disk_image   = var.db_disk_image
-  subnet_id       = var.yc_subnet_id
-  cores = 2
-  memory = 2
-  core_fraction = 20
+  disk_image     = var.db_disk_image
+  subnet_id      = var.yc_subnet_id
+  cores          = 2
+  memory         = 2
+  core_fraction  = 20
 }
--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -12,10 +12,10 @@ module "app" {
   ssh_private_key = var.ssh_private_key
   disk_image      = var.app_disk_image
   subnet_id       = var.yc_subnet_id
-  is_nat = true
-  cores = 4
-  memory = 4
-  core_fraction = 5
+  is_nat          = true
+  cores           = 4
+  memory          = 4
+  core_fraction   = 5
   db_host         = module.db.internal_ip_address_db
   db_port         = 27017
 }
