80af47b16f723d5663d192dfe14e7cdd757012be
ostin654 <ostin654@gmail.com>
2021-07-07 23:08:45 +0300

    Terraform modules, backend s3, provisioners (try #2 to fix tests)

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -5,13 +5,21 @@ provider "yandex" {
   zone                     = var.yc_zone
 }
 
+module "vpc" {
+  source          = "../modules/vpc"
+  network_name = "reddit-app-network"
+  subnet_name = "reddit-app-subnet"
+  zone = "ru-central1-a"
+  cidr = "192.168.10.0/24"
+}
+
 module "app" {
   name            = "reddit-app"
   source          = "../modules/app"
   ssh_public_key  = var.ssh_public_key
   ssh_private_key = var.ssh_private_key
   disk_image      = var.app_disk_image
-  subnet_id       = var.yc_subnet_id
+  subnet_id       = module.vpc.subnet_id
   is_nat          = true
   cores           = 4
   memory          = 4
@@ -25,7 +33,7 @@ module "db" {
   source         = "../modules/db"
   ssh_public_key = var.ssh_public_key
   disk_image     = var.db_disk_image
-  subnet_id      = var.yc_subnet_id
+  subnet_id      = module.vpc.subnet_id
   cores          = 2
   memory         = 2
   core_fraction  = 20
