8f1bb442a4d43fd11860b8158483a1b8d3b30ad7
ostin654 <ostin654@gmail.com>
2021-07-08 11:56:05 +0300

    Ansible examples, first playbook, dynamic inventory

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -5,6 +5,6 @@ resource "yandex_vpc_network" "network" {
 resource "yandex_vpc_subnet" "subnet" {
   name = var.subnet_name
   zone = var.zone
-  network_id = "${yandex_vpc_network.network.id}"
+  network_id = yandex_vpc_network.network.id
   v4_cidr_blocks = [var.cidr]
 }
