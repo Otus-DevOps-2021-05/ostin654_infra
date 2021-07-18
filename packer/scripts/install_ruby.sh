9c667fb55915859ddf06cb997e058a4b90f4a198
ostin654 <ostin654@gmail.com>
2021-07-18 14:12:48 +0300

    Ansible playbook for deploy, dynamic inventory, playbooks for packer

--- packer/scripts/install_ruby.sh
+++ packer/scripts/install_ruby.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sleep 30
+
 DEBIAN_FRONTEND=noninteractive apt-get remove -y unattended-upgrades
 DEBIAN_FRONTEND=noninteractive apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y ruby-full ruby-bundler build-essential
+DEBIAN_FRONTEND=noninteractive apt-get install -y git ruby-full ruby-bundler build-essential
